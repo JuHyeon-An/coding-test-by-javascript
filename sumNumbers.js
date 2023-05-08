5e63d9a789502f0ce926bb19912a6531e4144af8
JuHyeon-An <ajh2042@naver.com>
2023-05-08 16:38:57 +0900

    [프로그래머스]두 수의 합

---
+++ sumNumbers.js
@@ -0,0 +1,16 @@
+/* ==============================================================
+ * [코딩테스트 입문] 두 수의 합
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120802
+ *
+ * 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+ * ==============================================================
+ */
+
+function solution(num1, num2) {
+    return num1+num2;
+}
+
+console.log(solution(2,3));
+
+
+
