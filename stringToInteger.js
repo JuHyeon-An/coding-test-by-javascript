c9b7444a59c1ee34490b587d197e552b61946db8
JuHyeon-An <ajh2042@naver.com>
2023-05-08 17:32:54 +0900

    [프로그래머스]문자열을 정수로 변환하기

---
+++ stringToInteger.js
@@ -0,0 +1,13 @@
+/* ==============================================================
+ * [코딩 기초 트레이닝] 문자열을 정수로 변환하기
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/181848
+ *
+ * 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+ * ==============================================================
+ */
+
+function solution(n_str) {
+    return parseInt(n_str);
+}
+
+console.log(solution(3));
