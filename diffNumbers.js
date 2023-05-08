46f0aa81f884f678fe31591f937bbaed6fb84637
JuHyeon-An <ajh2042@naver.com>
2023-05-08 16:32:36 +0900

    [프로그래머스]두 수의 차 구하기

---
+++ diffNumbers.js
@@ -0,0 +1,16 @@
+/* ==============================================================
+ * [코딩테스트 입문] 두 수의 차
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120803
+ *
+ * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+ * ==============================================================
+ */
+
+function solution(num1, num2) {
+    return num1-num2;
+}
+
+console.log(solution(2,3));
+
+
+
