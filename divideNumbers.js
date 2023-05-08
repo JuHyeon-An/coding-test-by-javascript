cae4ccea9c23a7e1894cc15971b4682306321723
JuHyeon-An <ajh2042@naver.com>
2023-05-08 16:53:58 +0900

    [프로그래머스]두 수의 나눗셈

---
+++ divideNumbers.js
@@ -0,0 +1,13 @@
+/* ==============================================================
+ * [코딩테스트 입문] 두 수의 나눗셈
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120806
+ *
+ * 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
+ * ==============================================================
+ */
+
+function solution(num1, num2) {
+    return Math.floor((num1/num2)*1000);
+}
+
+console.log(solution(2,3));
