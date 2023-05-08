19edb2676d145a1e9f3351a35cbbc4927cd8a220
JuHyeon-An <ajh2042@naver.com>
2023-05-08 16:30:49 +0900

    [프로그래머스]몫 구하기

---
+++ getShare.js
@@ -0,0 +1,16 @@
+/* ==============================================================
+ * [코딩테스트 입문] 몫 구하기
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/120805
+ *
+ * 정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+ * ==============================================================
+ */
+
+function solution(num1, num2) {
+    return Math.floor(num1 / num2);
+}
+
+console.log(solution(2,3));
+
+
+
