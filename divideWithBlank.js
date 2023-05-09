3639e083f973e3796f1ba538ca9a071e0fad5807
JuHyeon-An <ajh2042@naver.com>
2023-05-09 16:59:06 +0900

    [프로그래머스]공백으로 구분하기

---
+++ divideWithBlank.js
@@ -0,0 +1,14 @@
+/* ==============================================================
+ * [코딩 기초 트레이닝] 공백으로 구분하기 1
+ * 출처 : https://school.programmers.co.kr/learn/courses/30/lessons/181869
+ *
+ * 단어가 공백 한 개로 구분되어 있는 문자열 my_string이 매개변수로 주어질 때, my_string에 나온 단어를 앞에서부터 순서대로 담은 문자열 배열을 return 하는 solution 함수를 작성해 주세요.
+ * ==============================================================
+ */
+
+const solution = my_string => my_string.split(' ')
+
+console.log(solution("i love you"));
+
+
+
